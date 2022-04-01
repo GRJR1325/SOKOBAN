9ff9571ddd2334abaed505ca295e659775d46a73
GRJR_1325 <97265804+GRJR1325@users.noreply.github.com>
2022-04-01 18:21:49 +0000

    Corrigiendo mov. derecha y izquierda

--- sokoban.py
+++ sokoban.py
@@ -18,54 +18,65 @@ class Sokoban:
 
 #CREACION DEL MAPA DE JUEGO
     
-    mapa =  [3,1,1,0,1,1,3] 
+    mapa =[
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 0, 1, 2, 1, 4, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        ]
           
     
-    posicion_columna= 3 #Posicion Inicial
-    posicion_fila= 1
+    posicion_columna= 4 #Posicion Inicial
+    posicion_fila= 4
 
     def __init__(self):
         pass
 
     def imprimirMapa(self):
-        for i in self.mapa:
-            if i == 0: #Muñeco
-                print(chr(64), end = "")
-            elif i == 1: #Espacio
-                print (" ",end = "")
-            elif i == 2: #Cajas
-                print(chr(164),end = "")
-            elif i == 3:  #Paredes
-                print (chr(220),end = "")
-            elif i == 4:  #Meta = m
-                print (chr(109),end = "")
-            elif i == 5:  #Muñeco_Meta = N
-                print (chr(78),end = "")
-            elif i == 6:  #Caja_Meta
-                print (chr(100),end = "")
-            else:
-                print(i, end = "")
-        print()
+        for row in self.mapa:  # For each row in map
+            print(row)
+       # for i in self.mapa:
+            #if i == 0: #Muñeco
+              #  print(chr(64), end = "")
+            #elif i == 1: #Espacio
+              #  print (" ",end = "")
+            #elif i == 2: #Cajas
+               # print(chr(164),end = "")
+            #elif i == 3:  #Paredes
+               # print (chr(220),end = "")
+            #elif i == 4:  #Meta = m
+               # print (chr(109),end = "")
+            #elif i == 5:  #Muñeco_Meta = N
+              #  print (chr(78),end = "")
+            #elif i == 6:  #Caja_Meta
+               # print (chr(100),end = "")
+            #else:
+                #print(i, end = "")
+        #print()
+        
 
 #CONFIGURACION DE MOVIMIENTOS
 
 #MOVIMIENTOS A LA DERECHA --->
     def moverDerecha(self):
-        if self.mapa [self.posiciion_fila] [self.posiciion_columna] == 0 and self.mapa[self.posiciion_fila][self.posicion_columna +1] == 1: #Calcula la nueva posicion del muñeco
+        if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila][self.posicion_columna+1] == 1: #Calcula la nueva posicion del muñeco
   
         #CONDICIONES--->
-            self.mapa [self.posicion_fila] [self.posicion_columna] = 1
-            self.mapa [self.posicion_fila] [self.posicion_columna+1] = 0
+            self.mapa[self.posicion_fila][self.posicion_columna] = 1
+            self.mapa[self.posicion_fila][self.posicion_columna+1] = 0
             self.posicion.columna += 1
             
 
 #MOVIMIENTOS A LA IZQUIERDA <---
     def moverIzquierda(self):
-        if self.mapa[self.posicion_columna] == 0 and self.mapa [self.posicion_columna - 1] == 1: #Calcula la nueva posicion del muñeco
+        if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila][self.posicion_columna - 1] == 1: #Calcula la nueva posicion del muñeco
 
             #CONDICIONES--->
-            self.mapa [self.posicion_fila] [self.posicion_columna] = 1
-            self.mapa [self.posicion_fila] [self.posicion_columna -1 ] = 0
+            self.mapa[self.posicion_fila][self.posicion_columna] = 1
+            self.mapa[self.posicion_fila][self.posicion_columna -1 ] = 0
             
             self.posicion.columna -= 1
 
