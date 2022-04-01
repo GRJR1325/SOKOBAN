987d97957d17e62695f79ae8f00128fdfe2d0fc8
GRJR_1325 <97265804+GRJR1325@users.noreply.github.com>
2022-04-01 19:58:38 +0000

    SIN AVANCES guardo notas en clase

--- sokoban.py
+++ sokoban.py
@@ -32,12 +32,10 @@ class Sokoban:
     posicion_columna= 4 #Posicion Inicial
     posicion_fila= 4
 
-    def __init__(self):
-        pass
-
+    
     def imprimirMapa(self):
-        for row in self.mapa:  # For each row in map
-            print(row)
+        for i in self.mapa:  # For each row in map
+            print(i)
        # for i in self.mapa:
             #if i == 0: #Muñeco
               #  print(chr(64), end = "")
@@ -56,16 +54,23 @@ class Sokoban:
             #else:
                 #print(i, end = "")
         #print()
-        
+
+     def actualizamapa (self):
+        for i in range(len(self.mapa)):  # Get rows number on the map
+            for columna in range(len(self.mapa[i])):  # Get columns number on the map
+                if self.mapa[fila][columna] == 0:  # If the character is found
+                    self.posicion_fila = fila # Update the character row position
+                    self.posicion_columna = columna           
 
 #CONFIGURACION DE MOVIMIENTOS
 
 #MOVIMIENTOS A LA DERECHA --->
     def moverDerecha(self):
-        if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila][self.posicion_columna+1] == 1: #Calcula la nueva posicion del muñeco
+        if (self.mapa[self.posicion_fila][self.posicion_columna] == 0 
+        and self.mapa[self.posicion_fila][self.posicion_columna+1] == 1): #Calcula la nueva posicion del muñeco
   
         #CONDICIONES--->
-            self.mapa[self.posicion_fila][self.posicion_columna] = 1
+            self.mapa[self.posicion_fila][self.posicion_columna] = 1     
             self.mapa[self.posicion_fila][self.posicion_columna+1] = 0
             self.posicion.columna += 1
             
@@ -103,14 +108,89 @@ while True:
         
 #PRUEBAS 
         
+#class Sokoban
+"""
+
+#mapa = {}
 
 
+    def __init__(self):
+        pass
+
+    def leerMapa(self):
+        self.mapa [
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 0, 1, 2, 1, 4, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        ]
+        self.personaje_fila = 4
+        self.personaje_coluna = 4 
+
+    def imprimirMapa(self):
+        for fila in self.mapa:
+            print(fila)
+
+    def moverDerecha(self)
+        print("Moverderecha")
+
+        if self.mapa [personaje_fila][personaje_columna] == 0 and
+            self.mapa [personaje_fila][personaje_columna+1]==1):
+
+si eso es cierto ejecuta :
 
 
-
-
-
-
-
-
+#donde estaba el personaje  
     
+
+
+def moverIzquierda(self)
+
+        print("Moverizquierda")
+
+
+
+
+    def jugar (self):
+        instruccioness= "A=izquierda"
+        print(INSTRUCCIONES)
+        self.leerMapa()
+            while TRUE:
+                self.imprimirMapa()
+                movimiento = input("Mover hacia")
+                if movimiento =="d";
+                    moverderecha()
+                elif movimiento =="a";
+                    moverizquierda()
+                elif movimiento =="w";
+                    moverdizquierda()
+                elif movimiento =="a";
+                    moverdizquierda()
+juego = Sokoban
+
+
+personaje meta
+        elif (self.mapa[self.posicion_fila][self.posicion_columna] == 0 
+        and self.mapa[self.posicion_fila][self.posicion_columna+1] == 4): #Calcula la nueva posicion del muñeco
+  
+        #CONDICIONES--->
+            self.mapa[self.posicion_fila][self.posicion_columna] = 1     donde estba queda
+            self.mapa[self.posicion_fila][self.posicion_columna+1] = 5    queda el personaje meta
+            self.posicion.columna += 1
+
+
+
+
+
+        if (self.mapa[self.posicion_fila][self.posicion_columna] == self.personaje   otra forma de hacerlo
+        and self.mapa[self.posicion_fila][self.posicion_columna+1] == self.espacio): #Calcula la nueva posicion del muñeco
+  
+        #CONDICIONES--->
+            self.mapa[self.posicion_fila][self.posicion_columna] = self.espacio 
+            self.mapa[self.posicion_fila][self.posicion_columna+1] = self.personaje
+            self.posicion.columna += 1
+
+juego = SOKOBAN
